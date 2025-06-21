24bc6c7682b8d73164511f1abe4724dc4669d80d
Laura Roberto <lauraroberto731@gmail.com>
2025-06-21 13:04:37 -0300

    Adição de exercicios

---
+++ OperacoesMatematicas/Calculos/Aposentadoria.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosComCsharp.OperacoesMatematicas.Calculos
+{
+    internal class Aposentadoria
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o ano atual: ");
+            int anoAtual = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o codigo do empregado: ");
+            int codigo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano quye você nasceu: ");
+            int anoNascimento = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano que você entrou na empresa: ");
+            int anoIngresso = int.Parse(Console.ReadLine());
+
+            int idade = anoAtual - anoNascimento;
+            int tempoTrabalho = anoAtual - anoIngresso;
+            
+            if(idade <= 65 || tempoTrabalho >= 30 || (idade >= 60 && tempoTrabalho >= 25))
+            {
+                Console.WriteLine("Requerer aposentadoria.");
+            }
+            else
+            {
+                Console.WriteLine("Não requerer aposentadoria.");
+            }
+            Console.WriteLine($"Idade: {idade} anos.");
+            Console.WriteLine($"Tempo trabalhado: {tempoTrabalho} anos.");
+        }
+    }
+}
---
+++ OperacoesMatematicas/Conversoes/CalculoGasolina.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosComCsharp.OperacoesMatematicas.Conversoes
+{
+    internal class CalculoGasolina
+    {
+        public static void Main(string[] args)
+        {
+            double precoLitro, desconto;
+
+            Console.WriteLine("Digite o tipo de combustível (A - Álcool ou G - Gasolina): ");
+            string tipoGasolina = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Digite a quantidade de litros: ");
+            double litros = double.Parse(Console.ReadLine());
+
+            if (tipoGasolina == "A")
+            {
+                precoLitro = 2.90;
+                if (litros <= 20)
+                {
+                    desconto = 0.03;
+                }
+                else
+                {
+                    desconto = 0.05;
+                }
+            }
+            else if (tipoGasolina == "G")
+            {
+                precoLitro = 3.30;
+                if (litros <= 20)
+                {
+                    desconto = 0.04;
+                }
+                else
+                {
+                    desconto = 0.06;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tipo de combustível inválido!");
+                return;
+            }
+
+            double valorBruto = litros * precoLitro;
+            double valorFinal = valorBruto - (valorBruto * desconto);
+
+            Console.WriteLine($"O valor final é R$ {valorFinal:F2}");
+        }
+    }
+}
